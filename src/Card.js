c553ae67b95e71a01483178ba611271fd5a1d34f
Dylan <dcren2010@gmail.com>
2020-01-10 15:44:49 -0500

    update

---
+++ src/Card.js
@@ -0,0 +1,20 @@
+import React, {Component} from "react";
+
+
+class Card extends Component {
+    render() {
+        return (
+            <div>
+                <img src="" alt=""/>
+                <div>
+                    <h2>Jane Doe</h2>
+                    <p>jane.doe@gmail.com</p>
+                </div>
+            </div>
+
+
+        )
+    }
+}
+
+export default Card
