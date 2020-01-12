45ca84087878dce9941df00384450efa95060bf0
Dylan <dcren2010@gmail.com>
2020-01-11 23:09:03 -0500

    Updates

---
+++ precache-manifest.cc64cc1f9243d210b36bcb19daa65ce9.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6e845e6f14fcd87593e0c8986daa3167",
+    "url": "/React-project-RobotFriends/index.html"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/React-project-RobotFriends/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "dc727a0317ada362329b",
+    "url": "/React-project-RobotFriends/static/css/main.2b98283c.chunk.css"
+  },
+  {
+    "revision": "c8890da5dfac99701af1",
+    "url": "/React-project-RobotFriends/static/js/2.a98415d0.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/React-project-RobotFriends/static/js/2.a98415d0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "dc727a0317ada362329b",
+    "url": "/React-project-RobotFriends/static/js/main.f6c13178.chunk.js"
+  },
+  {
+    "revision": "1d9c75b36e3eee719a85",
+    "url": "/React-project-RobotFriends/static/js/runtime-main.a673131c.js"
+  },
+  {
+    "revision": "82e09f7862a1e26fb7dfe1d0f8adc87f",
+    "url": "/React-project-RobotFriends/static/media/SEGA.82e09f78.TTF"
+  }
+]);
